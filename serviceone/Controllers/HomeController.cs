b7c32fc719172e1e07b64644f1592ead987042e5
Данила Глазунов <glazdan17@gmail.com>
2022-12-25 17:39:14 +0300

    Finish may be

--- serviceone/Controllers/HomeController.cs
+++ serviceone/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace serviceone.Controllers;
         public HomeController()
         {
             _client = new HttpClient();
-            _client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("URI_ENV") ?? string.Empty);
+            _client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("Weather")!);
             _client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
         }
